689ef26b4a2e351284f273471ad78b77f823a1a9
Ribeiro Thomas <tribeiro@auchan.fr>
2024-11-28 13:43:44 +0100

    setup project

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import express from "express";
+import mongoose from "mongoose";
+
+const PORT = 3500;
+
+/**
+ * Initialise le serveur Express avec les routes nécessaires.
+ */
+async function main() {
+  const server = express();
+  server.use(bodyParser.json());
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  try {
+    await mongoose.connect("mongodb://localhost:27017/monolithic-api", {});
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+}
+
+main();
